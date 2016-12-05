ad3497e377b51a76e8e769e6836f2275c766b92f
Ionică Bizău <bizauionica@gmail.com>
2016-12-05 12:11:51 +0200

    Initial prototype.

---
+++ DOCUMENTATION.md
@@ -0,0 +1,14 @@
+## Documentation
+
+You can see below the API reference of this module.
+
+### `safeRJson(a, b)`
+A convenient JSON file reader.
+
+#### Params
+- **Number** `a`: Param descrpition.
+- **Number** `b`: Param descrpition.
+
+#### Return
+- **Number** Return description.
+
--- example/index.js
+++ example/index.js
@@ -2,4 +2,19 @@
 
 const safeRJson = require("../lib");
 
-console.log(safeRJson());
+console.log(safeRJson("path/to/a/file/that/does/not/exist"));
+// => {}
+
+// If you expect an array, simply pass it there
+console.log(safeRJson("path/to/a/file/that/does/not/exist", []));
+// => []
+
+safeRJson("path/to/a/file/that/does/not/exist", [], (err, data) => {
+    console.log(err || data);
+    // => []
+});
+
+safeRJson(`${__dirname}/data.json`, (err, data) => {
+    console.log(data);
+    // => { hello: 'world' }
+});
